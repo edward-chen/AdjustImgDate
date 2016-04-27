3d3bcd57053f731d90247d83d2635c130e58ce33
edward.chen <jacy22@gmail.com>
2016-04-27 17:00:31 +0800

    Initial Commit

---
+++ AdjustImgDate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AdjustImgDate
+//
+//  Created by ChenYi-Hung on 2016/4/27.
+//  Copyright © 2016年 ChenYi-Hung. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AdjustImgDate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AdjustImgDate
+//
+//  Created by ChenYi-Hung on 2016/4/27.
+//  Copyright © 2016年 ChenYi-Hung. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
